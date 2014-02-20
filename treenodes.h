4204eecfffba9f5568db5e7ca8d51b5e299a0240
fjapa <florencioufs@gmail.com>
2014-02-19 23:31:54 -0200

    Update treenodes.h

--- treenodes.h
+++ treenodes.h
@@ -6,11 +6,39 @@
 */
 
 
+typedef struct PROG *tProgram;    /*programa*/
+typedef struct DECLIST *tDeclist; /*declaração-lista*/
+typedef struct DEC *tDec;         /*declaração*/
+typedef struct VARDEC *tDec;      /*var-declaração*/
+typedef struct TIPOESP *tTipoesp; /*tipo-especificador*/
+typedef struct FUNDEC *tFundec;   /*fun-declaração*/
+
 typedef struct S *SS;
 typedef struct params * tParams
 typedef struct var *tVar;
 
 typedef char *string;
+
+
+/* (1) programa —> declaração-lista*/
+struct PROG {
+	tDeclist declaracao-lista;
+};
+
+
+/* (2) declaração-lista —> declaração-lista declaração | declaração*/
+
+struct DECLIST{
+        enum {producao_declist, producao_declaracao} tipodeproducao;
+        union{	struct{
+			tDeclist declaracaoLista;
+			tDec declaracao;
+		} Tvariasdeclaracoes;
+		
+		tDec declaracao;
+
+	} uniao;
+};
 
 
 /*(7) params -> param-lista|void	*/
@@ -62,3 +90,35 @@ SS producao_S_abpr_S_fcpar(SS s);
 
 
 #endif
+
+/*
+programa —> declaração-lista
+declaração-lista —> declaração-lista declaração | declaração
+declaração —> var-declaração | fun-declaração
+var-declaração —» tipo-especificador ID ; | tipo-especificador ID [ NUM ] ;  
+tipo-especificador -> int | void 
+fun-declaração —> tipo-especificador ID ( params ) composto-decl
+params —> param-lista | void 
+param-lista —> param-lista, param | param 
+param —> tipo-especificador ID | tipo-especificador ID [] 
+composto-decl —> { local-declarações statement-lista 
+local-declarações -> local-declarações var-declaração | vazio
+statement-lista —> statement-lista statement | vazio
+statement —> expressão-decl | composto-decl | seleção-decl | iteração-decl | retorno-decl
+expressão-decl -> expressão ; | ;
+seleção-decl —> if ( expressão ) statement | if ( expressão ) statement else statement
+iteração-decl —> while ( expressão ) statement
+retorno-decl —> return ; | return expressão ;
+expressão -> var = expressão | simples-expressão
+var —> ID | I D [ expressão ]
+simples-expressão —> soma-expressão relacional soma-expressão | soma-expressão
+relacional -> <= | < | > | >= | » | !=
+soma-expressão —> soma-expressão soma termo | termo
+soma —> + | -
+termo —> termo multfator | fator
+mult -> * | /
+fator -> ( expressão ) | var | ativação | NUM
+ativação —> ID ( args )
+args —> arg-lista | vazio
+arg-lista -> arg-lista, expressão | expressão 
+*/
